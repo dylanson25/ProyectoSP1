e1db56739d937f4d34d5ff8ba6edf85632af022b
Dylanson25 <dvillarreal0@ucol.mx>
2022-05-23 09:23:23 -0500

    New screens
    The necessary screens to start dividing the routes of the application

---
+++ src/screens/CardProList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from '../assets/styleds';
+import {Text} from 'react-native';
+
+const CardProList = ({navigation}) =>  {
+    return(
+        <Container>
+            <Text>Lista de los profecionales</Text>
+        </Container>
+    )
+}
+export default CardProList;
